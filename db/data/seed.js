81eb9d6bb74015620ffd726370a12bdaa7ac6932
sahirabibi <sahirabibi96@gmail.com>
2021-10-06 17:29:05 -0400

    Add api url

--- db/data/seed.js
+++ db/data/seed.js
@@ -1,9 +1,7 @@
 const axios = require('axios')
 
 url =
-	'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=uDMFcggPK0EXdo1IqTFhAxh6jz2NXc0DoNEoa9t7';
+	`https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${process.env.API_KEY}`;
 axios.get(url)
     .then((res) => console.log(res.data))
     .catch(err => console.log(err));
-
-    

110f1564bbb829121cb707e2bdfe41b2f0725ea1
sahirabibi <sahirabibi96@gmail.com>
2021-10-06 22:16:43 -0400

    Pull and format data from API

--- db/data/seed.js
+++ db/data/seed.js
@@ -2,28 +2,20 @@ const axios = require('axios');
 
 url = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=uDMFcggPK0EXdo1IqTFhAxh6jz2NXc0DoNEoa9t7`;
 
-function getData(url) {
-	let results;
-	// GET DATA
-	axios.get(url)
+axios.get(url)
 		.then((res) => {
-			return results = res.data.data[0];
-		})
-		.then(() => {
-			let data = new FormatData(results);
-            return data;
-			// GET DATA
+			let results = res.data.data[0];
+            let data = new FormatData(results);
+            console.log(data);
 		})
 		.catch((err) => console.log(err));
-	// FORMAT DATA
-}
 
 class FormatData {
 	constructor(data) {
 		this.parkName = data.fullName;
 		this.parkDetails = data.description;
 		// this.image = this.getImages(data.images);
-		this.location = [...data.addresses];
+		this.locations = [...data.addresses];
 		this.latitude = data.latitude;
 		this.longitude = data.longitude;
 		this.activities = [...data.activities];
@@ -36,5 +28,3 @@ class FormatData {
 		}, []);
 	}
 }
-
-console.log(getData(url));
